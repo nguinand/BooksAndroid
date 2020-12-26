aa0ab92b2bdd2515ede8e11fbdf76cc225cbcaac
nguinand <nmguinand@hotmail.com>
2020-12-26 18:49:23 -0500

    Initial creation of splash screen dependencies

--- BooksProject/app/src/main/java/com/example/booksandroid/MainActivity.java
+++ BooksProject/app/src/main/java/com/example/booksandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.booksandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,11 +18,20 @@ import android.view.View;
 import com.example.booksandroid.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int SPLASH_TIME = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, SplashScreen.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
